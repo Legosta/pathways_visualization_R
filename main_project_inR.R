54a3aa6c8449ec09d81a926952015efc964d801f
legosta <stanislav.legkovoy@gmail.com>
2018-11-09 11:01:43 +0300

    commit#2

--- main_project_inR.R
+++ main_project_inR.R
@@ -9,10 +9,11 @@ library(clusterProfiler)
 source("https://bioconductor.org/biocLite.R")
 biocLite("clusterProfiler")
 
+
 cancerdata <- read.csv("CancerData.csv")
 cancerdata <- cancerdata[- grep("///", cancerdata $`Gene.ID.title`),] 
-
 rownames(cancerdata) <- cancerdata$`Gene.ID.title`
+
 can_to_pathview <- as.vector(cancerdata$logFC)
 names(can_to_pathview) <- rownames(cancerdata)
 
@@ -26,12 +27,16 @@ pv.out <- pathview(gene.data = can_to_pathview, gene.idtype = "KEGG",
                    pathway.id = chosen_pathway, species = species, out.suffix = chosen_pathway, keys.align = "y", 
                    kegg.native = F, sign.pos = "bottomleft", same.layer = T)
 
-data("kegg.gs")
-cancerdata_1 <- cancerdata[,-c(1:3)]
-cn_1=colnames(cancerdata_1)
+
+cancerdata_invert <- read.csv("CancerData_remastered.csv")
+cancerdata_invert <- cancerdata_invert[- grep("///", cancerdata_invert$`Gene.ID.title`),] 
+rownames(cancerdata_invert) <- cancerdata_invert$`Gene.ID.title`
+cancerdata_invert_2 <- cancerdata_invert[, -c(1:3)]
+cn_1=colnames(cancerdata_invert_2)
 lec_con <- grep('LEC.control',cn_1, ignore.case =TRUE)
 lec_vir <- grep('LEC.KSHV',cn_1, ignore.case =TRUE)
-cancer_1.kegg.p <- gage(cancerdata_1, gsets = kegg.gs,
+data("kegg.gs")
+cancer_1.kegg.p <- gage(cancerdata_invert_2, gsets = kegg.gs,
                         ref = lec_con, samp = lec_vir)
-cancer_1.kegg.2d.p <- gage(cancerdata_1, gsets = kegg.gs,
+cancer_1.kegg.2d.p <- gage(cancerdata_invert_2, gsets = kegg.gs,
                            ref = lec_con, samp = lec_vir, same.dir = FALSE)
